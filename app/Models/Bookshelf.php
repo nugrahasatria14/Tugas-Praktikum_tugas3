a1364544310727cd693f351adfbcd224f94af95f
nugrahasatria14 <124239310+nugrahasatria14@users.noreply.github.com>
2026-04-21 21:40:42 +0700

    Menambahkan models

---
+++ app/Models/Bookshelf.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Bookshelf extends Model
+{
+    protected $fillable = ['code', 'name'];
+
+    public function books()
+    {
+        return $this->hasMany(Book::class);
+    }
+    
+}
---
+++ app/Models/LoanDetail.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LoanDetail extends Model
+{
+     protected $table = 'loan_detail';
+
+    protected $fillable = [
+        'loan_id',
+        'book_id',
+        'is_return'
+    ];
+
+    public function loan()
+    {
+        return $this->belongsTo(Loan::class);
+    }
+
+    public function book()
+    {
+        return $this->belongsTo(Book::class);
+    }
+
+    public function return()
+    {
+        return $this->hasOne(ReturnModel::class, 'loan_detail_id');
+    }
+}
